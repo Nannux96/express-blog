4afba2af68e579a0f1d9b246622175b52387d7b8
Nicola Nannetti <nannetti@ldpgis.it>
2026-03-11 10:55:15 +0100

    fixes

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,40 +2,179 @@ const posts = require("../data/posts")
 const db = require("../db/index")
 
 /* ----------------- FUNZIONI CON DB --------------------- */
-// async function index(req, res) {
-// 	let results = [];
+async function index(req, res) {
+	let results = [];
 
-// 	// console.log(req.query)
-// 	const limit = req.query.limit ?? 10;
-// 	const page = req.query.page ?? 1;
-// 	const myQuery = "SELECT * FROM posts LIMIT $1 OFFSET $2"
+	// console.log(req.query)
+	const limit = req.query.limit ?? 10;
+	const page = req.query.page ?? 1;
+	const myQuery = "SELECT * FROM posts LIMIT $1 OFFSET $2"
 
-// 	try {
-// 		const risultati = await db.query(myQuery, [limit, page * limit - limit])
-// 		results = risultati.rows
-// 	} catch (err) {
-// 		console.log("Connection error: ", err.message)
+	const { rows, rowCount } = await db.query(myQuery, [limit, page * limit - limit])
+
+	const success = (rowCount > 0);
+	const statusCode = success ? 200 : 404;
+
+	const response = {
+		success,
+		payload: rows
+	};
+
+	return res.status(statusCode).json(response);
+
+}
+
+async function show(req, res) {
+	const id = parseInt(req.params.id)
+
+	let result = null;
+
+	const statement = `SELECT id, title, tags, date FROM posts WHERE id = $1`;
+	const params = [id];
+
+	const { rows, rowCount } = await db.query(statement, params);
+
+	const success = (rowCount > 0);
+	const statusCode = success ? 200 : 404;
+
+	const response = {
+		success,
+		payload: rows[0]
+	};
+
+	if (!success) {
+		response.error = "Not found";
+		response.message = "Articolo non trovata";
+	}
+
+	res.status(statusCode).json(response);
+}
+
+async function store(req, res) {
+	const { title, tags } = req.body;
+
+	if (!title || title.length < 3) {
+		throw new Error("Titolo non valido");
+	}
+
+	if (!Array.isArray(tags)) {
+		throw new Error("Tag non validi");
+	}
+
+	const statement = `INSERT INTO posts (title, tags) VALUES ($1, $2::jsonb) RETURNING *`;
+
+	const params = [title, JSON.stringify(tags)];
+
+	const { rows, rowCount } = await db.query(statement, params);
+
+	const success = (rowCount == 1);
+	const statusCode = success ? 201 : 404;
+
+	const response = {
+		success,
+		payload: rows[0]
+	};
+
+	if (!success) {
+		response.error = "Errore inserimento";
+		response.message = "Impossibile inserire l'elemento";
+	}
+
+	res.status(statusCode).json(response);
+}
+
+async function update(req, res) {
+	const id = Number(req.params.id);
+	const { title, tags } = req.body;
+
+	if (!title || title.length < 3) {
+		throw new Error("Titolo non valido");
+	}
+
+	if (!Array.isArray(tags)) {
+		throw new Error("Tag non validi");
+	}
+
+	if (!Number.isInteger(id) || id <= 0) {
+		throw new Error("ID non valido");
+	}
+
+	const statement = `UPDATE posts SET title=$1, tags=$2::json WHERE id=$3 RETURNING *`;
+
+	const params = [title, JSON.stringify(tags), id];
+
+	const { rows, rowCount } = await db.query(statement, params);
+
+	const success = (rowCount == 1);
+	const statusCode = success ? 201 : 404;
+
+	const response = {
+		success,
+		payload: rows[0]
+	};
+
+	if (!success) {
+		response.error = "Errore modifica";
+		response.message = "Impossibile modificare l'elemento";
+	}
+
+	res.status(statusCode).json(response);
+}
+async function modify(req, res) {
+}
+
+async function destroy(req, res) {
+
+	const id = Number(req.params.id);
+
+	if (!Number.isInteger(id) || id <= 0) {
+		throw new Error("ID non valido");
+	}
+
+	const statement = `DELETE FROM posts WHERE id=$1`;
+
+	const params = [id];
+
+	const { rows, rowCount } = await db.query(statement, params);
+
+	const success = (rowCount == 1);
+	const statusCode = success ? 201 : 404;
+
+	const response = {
+		success,
+		payload: rows[0]
+	};
+
+	if (!success) {
+		response.error = "Errore eliminazione";
+		response.message = "Impossibile eliminare l'elemento";
+	}
+
+	res.status(statusCode).json(response);
+}
+
+/* ----------------- FUNZIONI SENZA DB --------------------- */
+// function index(req, res) {
+// 	let results = posts;
+
+// 	if (req.query.tags) {
+
+// 		const needle = req.query.tags;
+// 		results = posts.filter(post => post.tags.includes(needle));
+
 // 	}
 
 // 	res.json(results);
-
 // }
 
-// async function show(req, res) {
+// function show(req, res) {
+
 // 	const id = parseInt(req.params.id)
 
-// 	let result = null;
+// 	const post = posts.find(p => p.id === id)
 
-// 	const myQuery = "SELECT * FROM posts WHERE id = $1"
 
-// 	try {
-// 		const risultati = await db.query(myQuery, [id])
-// 		result = risultati.rows[0]
-// 	} catch (err) {
-// 		console.log("Connection error: ", err.message)
-// 	}
-
-// 	if (!result) {
+// 	if (!post) {
 
 // 		res.status(404)
 
@@ -45,52 +184,62 @@ const db = require("../db/index")
 // 		})
 // 	}
 
-// 	res.json(result);
+// 	res.json(post)
 // }
 
-// async function store(req, res) {
-// 	// check campi obbligatori
+// function store(req, res) {
+// 	const postIdsList = posts.map(post => post.id)
+// 	const lastId = Math.max(...postIdsList)
+// 	const newId = lastId + 1;
+
+// 	const reqBody = req.body
+// 	const newPost = {
+// 		id: newId,
+// 		titolo: reqBody.titolo,
+// 		contenuto: reqBody.contenuto,
+// 		immagine: reqBody.immagine,
+// 		tags: reqBody.tags
+// 	}
+
+// 	posts.push(newPost)
+
+// 	res.status(201)
+// 	res.json(newPost)
+// }
+
+// function update(req, res) {
+
+// 	const id = parseInt(req.params.id)
+
+// 	const post = posts.find(p => p.id === id)
+
+// 	if (!post) {
+
+// 		res.status(404)
+
+// 		res.json({
+// 			error: "Not found",
+// 			message: "Post non trovato"
+// 		})
+// 	}
 // 	const reqBody = req.body
 
-// 	if (!reqBody.titolo || !reqBody.contenuto) {
-// 		res.status(400)
-// 		res.json({
-// 			error: "Bad request",
-// 			message: "Titolo e contenuto sono obbligatori"
-// 		})
-// 	}
+// 	post.titolo = reqBody.titolo
+// 	post.contenuto = reqBody.contenuto
+// 	post.immagine = reqBody.immagine
+// 	post.tags = reqBody.tags
 
-// 	// const myQuery = "INSERT INTO  id,username FROM users WHERE id = $1"
-
-// 	// try {
-// 	// 	const risultati = await db.query(myQuery, [id])
-// 	// 	result = risultati.rows[0]
-// 	// } catch (err) {
-// 	// 	console.log("Connection error: ", err.message)
-// 	// }
+// 	res.json(post)
 
 // }
-// async function update(req, res) {
-// }
-// async function modify(req, res) {
-// }
 
-// async function destroy(req, res) {
+// function modify(req, res) {
 
 // 	const id = parseInt(req.params.id)
 
-// 	let result = null;
+// 	const post = posts.find(p => p.id === id)
 
-// 	const myQuery = "DELETE FROM posts WHERE id = $1 RETURNING *"
-
-// 	try {
-// 		const risultati = await db.query(myQuery, [id])
-// 		result = risultati.rows[0]
-// 	} catch (err) {
-// 		console.log("Connection error: ", err.message)
-// 	}
-
-// 	if (!result) {
+// 	if (!post) {
 
 // 		res.status(404)
 
@@ -99,137 +248,38 @@ const db = require("../db/index")
 // 			message: "Post non trovato"
 // 		})
 // 	}
+// 	const reqBody = req.body
 
-// 	res.json(result);
+// 	post.titolo = reqBody.titolo ?? post.titolo
+// 	post.contenuto = reqBody.contenuto ?? post.contenuto
+// 	post.immagine = reqBody.immagine ?? post.immagine
+// 	post.tags = reqBody.tags ?? post.tags
+
+// 	res.json(post)
+
 // }
 
-/* ----------------- FUNZIONI SENZA DB --------------------- */
-function index(req, res) {
-	let results = posts;
+// function destroy(req, res) {
 
-	if (req.query.tags) {
+// 	const id = parseInt(req.params.id)
 
-		const needle = req.query.tags;
-		results = posts.filter(post => post.tags.includes(needle));
-
-	}
-
-	res.json(results);
-}
-
-function show(req, res) {
-
-	const id = parseInt(req.params.id)
-
-	const post = posts.find(p => p.id === id)
+// 	const post = posts.find(p => p.id === id)
 
 
-	if (!post) {
+// 	if (!post) {
 
-		res.status(404)
+// 		res.status(404)
 
-		res.json({
-			error: "Not found",
-			message: "Post non trovato"
-		})
-	}
+// 		res.json({
+// 			error: "Not found",
+// 			message: "Post non trovato"
+// 		})
+// 	}
+// 	const index = posts.indexOf(post)
 
-	res.json(post)
-}
+// 	posts.splice(index, 1);
 
-function store(req, res) {
-	const postIdsList = posts.map(post => post.id)
-	const lastId = Math.max(...postIdsList)
-	const newId = lastId + 1;
-
-	const reqBody = req.body
-	const newPost = {
-		id: newId,
-		titolo: reqBody.titolo,
-		contenuto: reqBody.contenuto,
-		immagine: reqBody.immagine,
-		tags: reqBody.tags
-	}
-
-	posts.push(newPost)
-
-	res.status(201)
-	res.json(newPost)
-}
-
-function update(req, res) {
-
-	const id = parseInt(req.params.id)
-
-	const post = posts.find(p => p.id === id)
-
-	if (!post) {
-
-		res.status(404)
-
-		res.json({
-			error: "Not found",
-			message: "Post non trovato"
-		})
-	}
-	const reqBody = req.body
-
-	post.titolo = reqBody.titolo
-	post.contenuto = reqBody.contenuto
-	post.immagine = reqBody.immagine
-	post.tags = reqBody.tags
-
-	res.json(post)
-
-}
-
-function modify(req, res) {
-
-	const id = parseInt(req.params.id)
-
-	const post = posts.find(p => p.id === id)
-
-	if (!post) {
-
-		res.status(404)
-
-		res.json({
-			error: "Not found",
-			message: "Post non trovato"
-		})
-	}
-	const reqBody = req.body
-
-	post.titolo = reqBody.titolo ?? post.titolo
-	post.contenuto = reqBody.contenuto ?? post.contenuto
-	post.immagine = reqBody.immagine ?? post.immagine
-	post.tags = reqBody.tags ?? post.tags
-
-	res.json(post)
-
-}
-
-function destroy(req, res) {
-
-	const id = parseInt(req.params.id)
-
-	const post = posts.find(p => p.id === id)
-
-
-	if (!post) {
-
-		res.status(404)
-
-		res.json({
-			error: "Not found",
-			message: "Post non trovato"
-		})
-	}
-	const index = posts.indexOf(post)
-
-	posts.splice(index, 1);
-
-	res.sendStatus(204)
-}
+// 	res.sendStatus(204)
+// }
 
 module.exports = { index, show, store, update, modify, destroy }
